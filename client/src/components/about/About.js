70ad9b133658f6889acdcc1ebbac465da911a1fc
marysonthego <marysonthego@gmail.com>
2022-10-10 22:32:25 -0600

    minor About changes

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -4,8 +4,8 @@ export const About = () => {
     <>
     <main className = 'aboutContainer'>
     <section className = 'aboutSection'>
-      <h1 className = "aboutTitle">Who is Mary?</h1>
-      <h2 className = "aboutSubtitle">A React full-stack developer looking for a job!</h2>
+      <h1 className = "aboutTitle">Who's Mary?</h1>
+      <h2 className = "aboutSubtitle">A React full-stack developer looking for a new project!</h2>
 
         <ul className = "ulTopCircle">
           <li className = "aboutText">Specializing in React, NodeJS, & Express</li>
@@ -14,24 +14,27 @@ export const About = () => {
         </ul>
 
       <h1 className = "aboutTitle">There's Something about Mary...</h1>
-      <h2 className = "aboutSubtitle">Hint: (In the business for 30 years)</h2>
+      <h2 className = "aboutSubtitle">Hint: (In the business for 30 years - also an old <a href="https://www.imdb.com/title/tt0129387/">movie</a>)</h2>
       <ul className = "ulReact">
-        <li className = "aboutText">Learning full-stack development <i>by doing</i> for more than 2 years. Has anyone ever learned <i>too much</i> about React?</li>
+        <li className = "aboutText">Learning full-stack development <i>by doing</i>. Has anyone ever learned <i>too much</i> about React?</li>
       </ul>
       <ul className = "ulCA">
-        <li className = "aboutText">CA Technologies</li>
+        <li className = "aboutText">CA Technologies 2007-2018</li>
       </ul>
       <ul className = "ulSubCircle">
-        <li className = "aboutSubText">Java programming</li>
+        <li className = "aboutSubText">Java programming with MySQL database replication</li>
         <li className = "aboutSubText">high-stakes tech support for Fortune 100 companies</li>
-        <li className = "aboutSubText">turned disorganized, incomplete documentation into clear, useful information.</li>
+        <li className = "aboutSubText">turned disorganized, incomplete documentation into clear, high-value information.</li>
       </ul>
       <ul className = "ulNortel">
-        <li className = "aboutText">Nortel Networks</li>
+        <li className = "aboutText">Nortel Networks 1998-2001</li>
       </ul>
       <ul className = "ulTI">
-        <li className = "aboutText">Texas Instruments</li>
+        <li className = "aboutText">Texas Instruments 1990-1998</li>
       </ul>
+      <h1 className = "aboutTitle">Why should Mary be on your project?</h1>
+      <h1 className = "aboutTitle">Ready for details? </h1>
+      <h2 className = "aboutSubtitle">Shoot me an email</h2>
     </section>
 </main>
 </>
